201d49bf6d64d826aae5e5261a615d33030dbdf6
Harshal Patil <harshal1232413@gmail.com>
2025-12-07 12:33:07 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("...........................Hello.............................")
+})
+app.use("/hello", (req, res) => {
+    res.send("Hello Hello Hello!")
+})
+
+app.use("/test",(req, res) => {
+    res.send("Hello from server")
+})
+
+app.listen(7000, () => {
+    console.log('Server is running on port 7000');
+});
