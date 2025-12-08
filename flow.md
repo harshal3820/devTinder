a80b31d85b43c247ae21d3462ac8b4f2a0862a24
Harshal Patil <harshal1232413@gmail.com>
2025-12-08 08:15:22 +0530

    exploring get, post , delete

---
+++ flow.md
@@ -0,0 +1,22 @@
+- Create a repository
+- Initialize the repository
+- node_modules, package.json, package_lock.json
+- Install express
+- create a server
+- Liste to port 7000
+- Write request handlers for /test, /hello
+- Install nodemon and update scripts inside package.json
+- What are dependancies
+- What is use of "-g" while npm install
+- Difference between caret and tilde (^ vs ~)
+
+
+- Initialize git
+- .gitignore
+- Create a remote repo on github
+- Push all code to remote origin
+- Play with routes and route extensions ex. - /hello, / , /hello/2, /xyz
+- order of the routes matter a lot
+- Install postman app and make a workspace/collection > test API calls
+- write logic to handle GET, POST, PATCH, DELETE API calls and test them on postman
+
--- src/app.js
+++ src/app.js
@@ -2,15 +2,31 @@ const express = require('express');
 
 const app = express();
 
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstName: "Harshal", lastName: "Patil"});
+})
+
+app.post("/user", (req, res) => {
+    console.log(req.body);
+    //Saving data to DB
+    res.send("Data successfully saved to db!");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully!");
+})
+
+//This will match all the HTTP method API calls to /test i.e. GET, POST, DELETE
+app.use("/test",(req, res) => {
+    res.send("Hello from server")
+})
+
 app.use("/", (req, res) => {
-    res.send("...........................Hello.............................")
+    res.send("Namaste Harshal")
 })
 app.use("/hello", (req, res) => {
     res.send("Hello Hello Hello!")
-})
-
-app.use("/test",(req, res) => {
-    res.send("Hello from server")
 })
 
 app.listen(7000, () => {
